990ba2de69a9b9ee4abd4f14009f7f9dab3f0d1f
sara burchi colonna <burchicolonnasara@gmail.com>
2025-03-12 17:29:54 +0100

    modifiedSchema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2025_03_12_140435) do
-
+ActiveRecord::Schema[7.1].define(version: 2025_03_12_140845) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -62,9 +60,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_12_140435) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "image_url"
     t.integer "price"
     t.string "picture"
+    t.string "image_url"
     t.boolean "status"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
