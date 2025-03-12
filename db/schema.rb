407c4c23309f9acaf12f7bfde11c0a9713a25a31
sara burchi colonna <burchicolonnasara@gmail.com>
2025-03-12 11:01:21 +0100

    Add column to our tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_11_112813) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_12_100029) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -33,6 +33,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_11_112813) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "price"
+    t.string "picture"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
@@ -44,6 +46,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_11_112813) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
+    t.string "phone_num"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
