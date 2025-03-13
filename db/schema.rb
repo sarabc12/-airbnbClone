cb97b3e583749ddde9e70ed189d26b72d7e849e2
Adam Smyth <adamss.smyth@gmail.com>
2025-03-13 12:51:48 +0100

    search bar complete

--- db/schema.rb
+++ db/schema.rb
@@ -61,9 +61,9 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_13_104159) do
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_url"
     t.integer "price"
     t.string "picture"
-    t.string "image_url"
     t.index ["user_id"], name: "index_flats_on_user_id"
   end
 
