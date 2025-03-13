2da13d62b178ca681e8987fed717774cfdbf81be
mario abd el ahad <mario_abdelahad@hotmail.com>
2025-03-13 11:52:09 +0100

    test

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,9 +1,8 @@
 class BookingsController < ApplicationController
-
   def index
     @bookings = Booking.all
   end
-  
+
   def new
     @flat = Flat.find(params[:flat_id])
     @booking = Booking.new
@@ -21,20 +20,18 @@ class BookingsController < ApplicationController
       puts @booking.errors.full_messages
       render :new # status: :unprocessable_entity
     end
-    
-   def destroy
-     @booking = Booking.find(params[:id])
-     @flat = @booking.flat
-     @booking.destroy
-     redirect_to flat_path(@flat)
-   end
+  end
+
+  def destroy
+    @booking = Booking.find(params[:id])
+    @flat = @booking.flat
+    @booking.destroy
+    redirect_to flat_path(@flat)
+  end
 
   private
 
   def bookings_params
     params.require(:booking).permit(:start_date, :end_date, :flat_id)
   end
-
- end
-
- 
+end
--- app/models/flat.rb
+++ app/models/flat.rb
@@ -6,5 +6,4 @@ class Flat < ApplicationRecord
   validates :title, presence: true
   validates :description, presence: true
   validates :address, presence: true
-  # validates :status, presence: true, inclusion: { in: [true, false] }
 end
